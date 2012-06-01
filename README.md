8acf7ad5fdbbdad972c42941c9c45ea2f181f5b9
Avaq <aldwin.vlasblom@gmail.com>
2012-06-01 13:07:39 +0200

    Created the class factory. * Ported all modules over from my previous attempt at making a Javascript framework. They are in .gitignore for now. * Rewrote Class.js. * Added the license. * Updated the README. * Added some libraries. * Added a test page.

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-dollar.mvc
-==========
+# Dollar.MVC
 
-Another Javascript MVC framework that makes use Underscore.js and jQuery or Zepto.
+## Introduction
+
+Another JavaScript MVC framework that makes use Underscore.js and jQuery or Zepto.
+
+This is the framework that will be used in the
+[Tuxion.Framework](https://github.com/Tuxion/tuxion.framework).
+
+## License
+
+Copyright (c) 2012 Avaq, https://github.com/Avaq
+
+Dollar.MVC is licensed under the MIT license. The license is included as
+[LICENSE in this directory](https://github.com/Avaq/dollar.mvc/blob/master/LICENSE).
---
+++ src/Class.js
@@ -0,0 +1,96 @@
+;(function(window, _, $, undefined){
+  
+  var Class = $.mvc.Class = function(){
+    
+    //Define a class-factory and an actual class for this "class".
+    var ClassFactory = {}
+      , noop = function(){}
+      , Class = noop;
+      
+    //Extend the factory with function that can extend the class.
+    _(ClassFactory).extend({
+      
+      //Set a parent class.
+      extends: function(parent){
+        
+        //Add the statics of our parent to our statics.
+        Class.__proto__ = parent;
+        
+        //Add the prototype of our parent to our own prototype.
+        Class.prototype.__proto__ = parent.prototype;
+        
+        //If we haven't defined our own constructor, use a function that will call the parent.
+        if(Class.toString() == noop.toString()){
+          this.construct(function(){
+            parent.apply(this, arguments);
+          });
+        }
+        
+        //Keep an easy reference to parent.
+        Class.prototype.parent = parent;
+        
+        //Enable chaining.
+        return this;
+        
+      },
+      
+      //Add static members to the class.
+      statics: function(map){
+        
+        //Add the members in the map to the Class object.
+        _(Class).extend(map);
+        
+        //Enable chaining.
+        return this;
+        
+      },
+      
+      //Change the constructor (default is a noop).
+      construct: function(constructor){
+        
+        //Remember the old class properties.
+        var oldMembers = Class.prototype || {};
+        var oldStatics = _({}).extend(Class);
+        
+        //Set the new constructor.
+        Class = constructor;
+        Class.prototype = oldMembers;
+        _(Class).extend(oldStatics);
+        
+        //Add a reference to the classes static members from within the classes normal members.
+        _(Class.prototype).extend({
+          static: Class
+        });
+        
+        //Enable chaining.
+        return this;
+        
+      },
+      
+      //Add normal members to the class.
+      members: function(map){
+        
+        //Add the members in the map to the Class's prototype.
+        _(Class.prototype).extend(map);
+        
+        //Enable chaining.
+        return this;
+        
+      },
+      
+      //Returns the class (Also returned by the new keyword).
+      finalize: function(){
+        return Class;
+      }
+      
+    });
+    
+    //Add a default noop constructor.
+    ClassFactory.construct(function(){});
+    
+    //Return the class-factory.
+    return ClassFactory;
+    
+  }
+  
+})(window, _, (jQuery || Zepto || $ || ($={})));
