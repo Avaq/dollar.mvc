391f00128cac819b5362a64e3bdb7414fac2276a
Avaq <aldwin.vlasblom@gmail.com>
2012-06-04 11:34:13 +0200

    Separated the ClassFactory from the Class creation function and exposed it. So that it may be extended.

--- src/Class.js
+++ src/Class.js
@@ -1,95 +1,99 @@
 ;(function(window, _, $, undefined){
   
-  var Class = $.mvc.Class = function(){
+  var ClassFactory = $.mvc.ClassFactory = function(Class){
+    this.Class = this.noop = Class;
+  }
+  
+  //Extend the factory with functions that can extend the class.
+  _(ClassFactory.prototype).extend({
     
-    //Define a class-factory and an actual class for this "class".
-    var ClassFactory = {}
-      , noop = function(){}
-      , Class = noop;
+    //Set a parent class.
+    extends: function(parent){
       
-    //Extend the factory with function that can extend the class.
-    _(ClassFactory).extend({
+      //Add the statics of our parent to our statics.
+      this.Class.__proto__ = this.Class.parent = parent;
       
-      //Set a parent class.
-      extends: function(parent){
-        
-        //Add the statics of our parent to our statics.
-        Class.__proto__ = parent;
-        
-        //Add the prototype of our parent to our own prototype.
-        Class.prototype.__proto__ = parent.prototype;
-        
-        //If we haven't defined our own constructor, use a function that will call the parent.
-        if(Class == noop){
-          this.construct(function(){
-            parent.apply(this, arguments);
-          });
-        }
-        
-        //Keep an easy reference to parent.
-        Class.prototype.parent = parent;
-        
-        //Enable chaining.
-        return this;
-        
-      },
+      //Add the prototype of our parent to our own prototype.
+      this.Class.prototype.__proto__ = parent.prototype;
       
-      //Add static members to the class.
-      statics: function(map){
-        
-        //Add the members in the map to the Class object.
-        _(Class).extend(map);
-        
-        //Enable chaining.
-        return this;
-        
-      },
-      
-      //Change the constructor (default is a noop).
-      construct: function(constructor){
-        
-        //Remember the old class properties.
-        var oldMembers = Class.prototype || {};
-        var oldStatics = _({}).extend(Class);
-        
-        //Set the new constructor.
-        Class = constructor;
-        Class.prototype = oldMembers;
-        _(Class).extend(oldStatics);
-        
-        //Add a reference to the classes static members from within the classes normal members.
-        _(Class.prototype).extend({
-          static: Class
+      //If we haven't defined our own constructor, use a function that will call the parent.
+      if(this.Class == this.noop){
+        this.construct(function(){
+          parent.apply(this, arguments);
         });
-        
-        //Enable chaining.
-        return this;
-        
-      },
-      
-      //Add normal members to the class.
-      members: function(map){
-        
-        //Add the members in the map to the Class's prototype.
-        _(Class.prototype).extend(map);
-        
-        //Enable chaining.
-        return this;
-        
-      },
-      
-      //Returns the class.
-      finalize: function(){
-        return Class;
       }
       
-    });
+      //Keep an easy reference to parent.
+      this.Class.prototype.parent = parent;
+      
+      //Enable chaining.
+      return this;
+      
+    },
     
+    //Add static members to the class.
+    statics: function(map){
+      
+      //Add the members in the map to the Class object.
+      _(this.Class).extend(map);
+      
+      //Enable chaining.
+      return this;
+      
+    },
+    
+    //Change the constructor (default is a noop).
+    construct: function(constructor){
+      
+      //Remember the old class properties.
+      var oldMembers = this.Class.prototype || {};
+      var oldStatics = _({}).extend(this.Class);
+      
+      //Set the new constructor.
+      this.Class = constructor;
+      this.Class.prototype = oldMembers;
+      _(this.Class).extend(oldStatics);
+      
+      //Add a reference to the class' static members from within the class' normal members.
+      _(this.Class.prototype).extend({
+        static: this.Class
+      });
+      
+      //Enable chaining.
+      return this;
+      
+    },
+    
+    //Add normal members to the class.
+    members: function(map){
+      
+      //Add the members in the map to the Class' prototype.
+      _(this.Class.prototype).extend(map);
+      
+      //Enable chaining.
+      return this;
+      
+    },
+    
+    //Returns the class.
+    finalize: function(){
+      return this.Class;
+    }
+    
+  });
+
+  var Class = $.mvc.Class = function(){
+    
+    //Define the class and its factory.
+    var noop = function(){}
+      , Class = noop
+      , factory = new ClassFactory(Class);
+      
     //Add a default noop constructor.
-    ClassFactory.construct(noop);
+    factory.construct(noop);
     
     //Return the class-factory.
-    return ClassFactory;
+    return factory;
     
   }
   
